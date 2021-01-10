76a7ab7c93979e7357f4fab7bfd1f94d79ff8275
anubhavcu <anubhav342y@gmail.com>
2021-01-10 20:46:12 +0530

    minor changes
    - added time and space complexity of fib.js

--- Dynamic-Programming/fib.js
+++ Dynamic-Programming/fib.js
@@ -1,9 +1,11 @@
+// O(2^n) time | O(n) space
 // var fib = (n) => {
 //   if (n <= 2) return 1;
 //   return fib(n - 1) + fib(n - 2);
 // };
 
 // using memoization
+// O(n) time  | O(n) space
 var fib = (n, memo = {}) => {
   if (n in memo) return memo[n];
   if (n <= 2) return 1;
